d924c0d9e1efae3fbb8aaaa7077d75ad998d6429
José Pablo Alpaca Rivera <jose.p.alpaca@gmail.com>
2022-02-04 18:13:54 -0500

    add seeds

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 140 }
 
   belongs_to :user
   has_many :comments, dependent: :destroy
